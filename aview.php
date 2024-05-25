a22b910a85202ee85e2eb24f9674efa7ad969823
Diksha More <140229106+diksha702042@users.noreply.github.com>
2024-05-25 14:40:01 +0530

    Add files via upload

---
+++ aview.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+<style>
+ul {
+  
+  margin: 0px;
+  padding: 0px;
+  overflow: hidden;
+  background-color:black;
+}
+
+li {
+  float: left;
+}
+
+/*table{
+background : transparent;
+backdrop-filter : blur(10px);
+transition : all . 3s ease-in;
+}
+*/
+li {
+  display: block;
+  color: white;
+
+}
+ a{
+  display: block;
+  color: white;
+  padding: 14px;
+  margin:14px;
+  text-decoration: none;
+}
+li a:hover {
+  background-color: green;
+}
+body{
+  background-image: url('5.jpg');
+  background-repeat:no-repeat;
+  background-size:100% 100%;
+  margin:0px;
+  padding:0px;
+}
+#tab1{
+ // margin:80px;
+  padding:50px;
+  width:80%;
+}
+
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a id="l1" href="index-.php">Home</a></li>
+  <li><a href="signin3.php">Signin users</a></li>
+  <li><a href="adminorders.php">Orders</a></li>
+ 
+</ul>
+
+</body>
+</html>
+
+
